8a35f13cce72b6c3c2f08f23e7c15ea578ca3200
root <root@TheMatrix.localdomain>
2022-05-17 09:46:12 +0800

    update: favicon and site config

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // social usernames (optional)
   twitter: 'Qiutingy',
   github: 'pidb',
-  // linkedin: 'yisheng-cai-42b518232',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
